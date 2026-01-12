eba4047b2244bec659086ccd58fa9a6d85e58a1b
zbpk <73913920+zbpk@users.noreply.github.com>
2026-01-11 21:48:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: '/<repo-name>/',   // <-- REQUIRED FOR GITHUB PAGES
+
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -19,6 +21,7 @@ export default defineConfig({
         ]
       : []),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
@@ -26,11 +29,14 @@ export default defineConfig({
       "@assets": path.resolve(import.meta.dirname, "attached_assets"),
     },
   },
+
   root: path.resolve(import.meta.dirname, "client"),
+
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
+
   server: {
     fs: {
       strict: true,
