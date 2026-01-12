fa9435b40b7322f2ea951169924a7a8c38e65ebf
zbpk <73913920+zbpk@users.noreply.github.com>
2026-01-11 21:50:39 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: '/<Samfolio-Hub>/',   // <-- REQUIRED FOR GITHUB PAGES
+  base: '/Samfolio-Hub/',   // REQUIRED FOR GITHUB PAGES
 
   plugins: [
     react(),
